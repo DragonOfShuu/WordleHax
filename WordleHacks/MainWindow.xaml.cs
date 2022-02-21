51441ee39c9db2ec3a14a489ccfc449014b85ff5
MushroomEnder <enderpie2005@gmail.com>
2022-02-21 09:42:52 -0800

    Added Basic UI
    Added a basic UI; no C# yet, but C# will be in there in the next update.

--- WordleHacks/MainWindow.xaml.cs
+++ WordleHacks/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
+// dynamic magic = JsonConvert.DeserializeObject(jsonStr);
 
 namespace WordleHacks
 {
